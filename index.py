f93d39b5589323fa534f78cc2db188393f24da6c
Richard Channing <richardsobelnkulu@gmail.com>
2022-05-17 20:20:12 +0200

    Adding some code on the project

--- index.py
+++ index.py
@@ -66,3 +66,14 @@ elif (ya%4 == 0):
     print("%d is a Leap year" %ya)
 else:
     print("%d is Not" %ya)
+    
+# Python Program to find Power of a Number
+
+number = int(input(" Please Enter any Positive Integer : "))
+exponent = int(input(" Please Enter Exponent Value : "))
+power = 1
+
+for i in range(1, exponent + 1):
+    power = power * number
+    
+print("The Result of {0} Power {1} = {2}".format(number, exponent, power))
